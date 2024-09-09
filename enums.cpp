555501c8d04a6b3a49be00ed3849dec1c9b5c216
Eldhose Babu <eldhosebabu197@gmail.com>
2024-09-09 13:49:52 -0400

    enums

---
+++ enums.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <array>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+enum Day {
+    sunday = 0, monday = 1, tuesday = 2, wednesday = 3, thursday = 4, friday = 5, saturday = 6
+};
+int main(){
+    //enums - a user-defined data type that consists of paired named-integer constants. Great if you have a set of potential options
+    Day today = sunday;
+
+    switch (today)
+    {
+    case 0:
+        cout << "It's sunday" << endl;
+        break;
+    case 1:
+        cout << "It's monday" << endl;
+        break;
+    case 2:
+        cout << "It's tuesday" << endl;
+        break;
+    case 3:
+        cout << "It's wednesday" << endl;
+        break;
+    case 4:
+        cout << "It's thursday" << endl;
+        break;
+    case 5:
+        cout << "It's friday" << endl;
+        break;
+    case 6:
+        cout << "It's saturday" << endl;
+        break;
+    default:
+        break;
+    }
+    return 0;
+}
