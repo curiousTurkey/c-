b6a1e45e26dcd3c51a58750bd153960750c6b975
Eldhose Babu <eldhosebabu197@gmail.com>
2024-09-09 13:40:03 -0400

    updated gitignore and updated struct passing in address method

--- passing_struct_as_args.cpp
+++ passing_struct_as_args.cpp
@@ -10,7 +10,8 @@ struct Car{
     int year;
     string color;
 };
-void carDetails(Car car);
+void carDetails(Car &car);
+void paintCar(Car &car, string color);
 int main(){
     Car car1, car2;
     car1.model = "LFA";
@@ -19,12 +20,23 @@ int main(){
     car2.model = "Camaro";
     car2.year = 2016;
     car2.color = "Yellow";
-    cout << "Car 1: ";
+    cout << "Car 1: " << endl;
     carDetails(car1);
-    cout << "Car 2: ";
+    cout << "Car 2: " << endl;
     carDetails(car2);
+    paintCar(car2, "Black");
     return 0;
 }
-void carDetails(Car car){
+void carDetails(Car &car){
     cout << car.model << endl << car.year << endl << car.color << endl;
+    cout << endl << endl;
 }
+void paintCar(Car &car, string color){
+    car.color = color;
+    cout << "Car painted to " << color << endl;
+    carDetails(car);
+}
+//can pass the address of the struct if u want to work with the original struct
+// void carDetails(Car &car){
+//     cout << car.model << endl << car.year << endl << car.color << endl;
+// }
